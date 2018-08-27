525ada840c1e1e05f70f4d9c288b3f9ded2fa691
LEE WEI WEN <weiwenl.justin@gmail.com>
2018-08-27 22:24:52 +0800

    Create game logic using js to display card img

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,8 +31,10 @@ var checkForMatch = function() {
     alert("Sorry, try again.");
   }
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+  cardId = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
   checkForMatch();
   }
@@ -41,5 +43,14 @@ var flipCard = function(cardId) {
   console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
+createBoard();
