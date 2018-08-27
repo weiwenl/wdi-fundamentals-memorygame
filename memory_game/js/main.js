7b542c0e35e2a69e9e5d36345eb9cb7087459ae0
LEE WEI WEN <weiwenl.justin@gmail.com>
2018-08-28 03:11:52 +0800

    Added comments and part of reset func

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,6 @@
 //console.log("Up and running!");
 
+// Memory cards identities
 var cards = [
   {
     rank: 'queen',
@@ -22,8 +23,11 @@ var cards = [
     cardImage: 'images/king-of-diamonds.png'
   }
 ];
+
+//Initialise user input to an empty array
 var cardsInPlay = [];
 
+//Check user selected cards for match and display message
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
@@ -31,6 +35,8 @@ var checkForMatch = function() {
     alert("Sorry, try again.");
   }
 }
+
+//When cards are click, this will flip over the cards and display cards value
 var flipCard = function() {
   cardId = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardId].rank);
@@ -38,11 +44,15 @@ var flipCard = function() {
   if (cardsInPlay.length === 2) {
   checkForMatch();
   }
+  if (cardsInPlay.length === 4) {
+  checkForMatch();
+  }
   console.log("User flipped " + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
 }
 
+//Setting up memory game board
 var createBoard = function () {
   for (var i = 0; i < cards.length; i++) {
     var cardElement = document.createElement('img');
@@ -53,4 +63,21 @@ var createBoard = function () {
   }
 }
 
+//Reset the game; triggered when button is clicked
+var resetGame = function () {
+  //Upon button press this function will reset game
+  //Set cardsInPlay array to " "
+  //Clear cardImage and set it to image/back
+  //Or should I remove cardElement
+  //Run createBoard()
+  for (var i = 0; i < cards.length; i++) {
+
+    cardElement.setAttribute('src', 'images/back.png');
+
+    //cardElement.splice(i, 4);
+  }
+  cardsInPlay = [];
+}
+
+//Start the game for the first time
 createBoard();
