80b76e2cefbdd44442c8093f869137a8c3daee05
LEE WEI WEN <weiwenl.justin@gmail.com>
2018-08-27 14:15:01 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,17 +3,19 @@ console.log("Up and running!");
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1])
-    alert("You found a match!");
-  else
-    alert("Sorry, try again.")
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
+}
+var flipCard = function(cardId) {
+  cardsInPlay.push(cards[cardId]);
+
+  checkForMatch();
+  console.log("User flipped " + cards[cardId]);
 }
 
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
+flipCard(0);
+flipCard(2);
